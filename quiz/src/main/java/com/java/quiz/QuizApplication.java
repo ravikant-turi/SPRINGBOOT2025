0f31d7b8fd01b77cb3c0c6cb0ff3d193bdf11e02
Ravikant Turi <ravikantt@infinite.com>
2025-10-13 18:39:21 +0530

    update

--- quiz/src/main/java/com/java/quiz/QuizApplication.java
+++ quiz/src/main/java/com/java/quiz/QuizApplication.java
@@ -9,6 +9,7 @@ public class QuizApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(QuizApplication.class, args);
+
 	}
 
 }
--- quiz/src/main/java/com/java/quiz/payload/QuestionDTO.java
+++ quiz/src/main/java/com/java/quiz/payload/QuestionDTO.java
@@ -1,7 +1,6 @@
 package com.java.quiz.payload;
 
 import com.java.quiz.entity.DifficultyLevel;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,15 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuestionDTO {
-
 	@NotBlank(message = "Question content must not be blank")
 	@Size(max = 500, message = "Question content must not exceed 500 characters")
 	@Size(min = 2, message = "Question content must be between 2 and 500 characters")
 	private String content;
-
 	@NotBlank(message = "Option A must not be blank")
 	private String optionA;
-
 	@NotBlank(message = "Option B must not be blank")
 	private String optionB;
 
@@ -40,7 +36,6 @@ public class QuestionDTO {
 	@NotNull(message = "Question number must not be null")
 	private String questionNumber;
 
-	
 	@NotNull(message = "DeficultyLevel number must not be null")
 	private DifficultyLevel deficultyLevel;
 }
